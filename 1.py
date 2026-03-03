a458a1bec9a560fca05d6976e09c5a30390cdcd1
aliayaz2110-create <aliayaz2110@gmail.com>
2026-03-03 19:52:58 +0000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+my_dict = {}
+my_dict["a bed"] = "where you can sleep"
+my_dict["a car"] = "where you can drive to places"
+my_dict["a phone"] = "where you can watch videos on"
+my_dict["a water bottle"] = "where water is stored"
+
+my_dict.pop("a bed")
+
+for key in my_dict:
+    print(key)
