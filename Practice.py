4224a843f5f46b1a536795821f6c6cccd8d8c0d9
aliayaz2110-create <aliayaz2110@gmail.com>
2026-06-20 12:53:08 +0100

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,17 @@
+people = {}
+
+people['Ayaz'] = 16
+people['Ali'] = 13
+people['Tom'] = 19
+people['Sara'] = 16
+people['Jeff'] = 20
+
+print(people)
+
+people['Tom'] = 17
+
+print(people)
+
+del people['Jeff']
+
+print(people)
